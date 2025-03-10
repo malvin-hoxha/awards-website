f76bbb8f0400d275a87034e6af014d47c7c092a6
MalvinHoLearning <malvinholearning@gmail.com>
2025-03-10 23:57:46 +0200

    Initialize Hero Section

---
+++ src/components/Hero.jsx
@@ -0,0 +1,81 @@
+import { useState, useRef } from "react"
+
+const Hero = () => {
+    const [currentIndex, setCurrentIndex] = useState(1);
+    const [hasClicked, setHasClicked] = useState(false);
+    const [isLoading, setIsLoading] = useState(true);
+    const [loadedVideos, setloadedVideos] = useState(0);
+
+    const totalVideos = 3;
+    const nextVideoRef = useRef(null);
+
+    const handleVideoLoad = () => {
+        setloadedVideos((prev) => prev + 1);
+    }
+
+    const upcomingVideoIndex = (currentIndex % totalVideos) + 1;
+    
+    const handeleMiniVdClick = () => {
+        setHasClicked(true);
+
+        setCurrentIndex(upcomingVideoIndex);
+    }
+
+    const getVideoSrc = (index) => `videos/hero-${index}.mp4`;
+
+    return (
+        <div className="relative h-dvh w-screen
+        overflow-x-hidden">
+            <div id="video-frane" className="relative z-10 h-dvh w-screen
+            overflow-hidden rounded-lg bg-blue-75">
+                <div>
+                    <div className="mask-clip-path absolute-center absolute
+                    z-50 cursor-pointer overflow-hidden rounded-lg">
+                        <div onClick={handeleMiniVdClick} className="origin-center
+                        scale-50 opacity-0 transition-all duration-500 ease-in
+                        hover:opacity-100 hover:scale-100">
+                            <video 
+                                ref={nextVideoRef}
+                                src={getVideoSrc(upcomingVideoIndex)}
+                                loop
+                                muted
+                                id="current-video"
+                                className="size-64 origin-center scale-150
+                                object-cover object-center"
+                                onLoadedData={handleVideoLoad}
+                            />
+                        </div>
+                    </div>
+                    <video
+                        ref={nextVideoRef}
+                        src={getVideoSrc(currentIndex)}
+                        loop
+                        muted
+                        id="next-video"
+                        className="absolute-center invisible absolute
+                        z-20 size-64 object-cover object-center"
+                        onLoadedData={handleVideoLoad}
+                    />
+
+                    <video 
+                        src={getVideoSrc(currentIndex)}
+                        autoPlay
+                        loop
+                        muted
+                        className="absolute left-0 top-0 size-full
+                        object-cover object-center"
+                        onLoadedData={handleVideoLoad}
+                    />
+
+                    <h1 className="absolute special-font hero-heading
+                    bottom-5 right-5 z-40 text-blue-75">
+                            G<b>A</b>MING
+                    </h1>
+
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Hero
